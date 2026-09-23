6c398c66dab18bec7bdfa238ad4646ace3bc654b
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-24 06:33:33 +0800

    Use one report definition source

--- application/libraries/Report_rules.php
+++ application/libraries/Report_rules.php
@@ -5,12 +5,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Report_rules {
 
     private $definitions = array(
-        'inventory' => array('title' => 'Inventory Report', 'type' => NULL),
-        'stock-in' => array('title' => 'Stock-In Report', 'type' => 'stock_in'),
-        'stock-out' => array('title' => 'Stock-Out Report', 'type' => 'stock_out'),
-        'movement' => array('title' => 'Stock Movement Report', 'type' => NULL),
-        'low-stock' => array('title' => 'Low-Stock Report', 'type' => NULL),
-        'valuation' => array('title' => 'Inventory Valuation', 'type' => NULL)
+        'inventory' => array('title' => 'Inventory Report', 'type' => NULL, 'method' => 'get_inventory_report'),
+        'stock-in' => array('title' => 'Stock-In Report', 'type' => 'stock_in', 'method' => 'get_stock_movement_report'),
+        'stock-out' => array('title' => 'Stock-Out Report', 'type' => 'stock_out', 'method' => 'get_stock_movement_report'),
+        'movement' => array('title' => 'Stock Movement Report', 'type' => NULL, 'method' => 'get_stock_movement_report'),
+        'low-stock' => array('title' => 'Low-Stock Report', 'type' => NULL, 'method' => 'get_low_stock_report'),
+        'valuation' => array('title' => 'Inventory Valuation', 'type' => NULL, 'method' => 'get_inventory_report')
     );
 
     public function get($report) {
