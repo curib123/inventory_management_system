c273d85ea45a6efcbd1918a0ce4b0abbb11ad263
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-24 21:49:02 +0800

    Add centralized component: application/config/inventory.php

---
+++ application/config/inventory.php
@@ -0,0 +1,27 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Product units
+|--------------------------------------------------------------------------
+| Keep units centralized so forms and future inventory screens use the same
+| values. Add or remove a unit here instead of editing individual views.
+*/
+$config['product_units'] = array(
+    'pcs' => 'Piece (pcs)',
+    'box' => 'Box',
+    'pack' => 'Pack',
+    'bottle' => 'Bottle',
+    'can' => 'Can',
+    'bag' => 'Bag',
+    'roll' => 'Roll',
+    'set' => 'Set',
+    'pair' => 'Pair',
+    'kg' => 'Kilogram (kg)',
+    'g' => 'Gram (g)',
+    'l' => 'Liter (L)',
+    'ml' => 'Milliliter (mL)',
+    'm' => 'Meter (m)'
+);
