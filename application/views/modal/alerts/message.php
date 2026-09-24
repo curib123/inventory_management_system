9b2a2ee3cb7900fab7fb9e611ccbe9717c57dd79
curib123 <studioanime34@gmail.com>
2026-09-24 16:30:08 +0800

    initial design

--- application/views/modal/alerts/message.php
+++ application/views/modal/alerts/message.php
@@ -1,8 +1,9 @@
-<dialog id="application-alert">
-    <h3><?php echo html_escape($alert_title); ?></h3>
-    <p><?php echo html_escape($alert_message); ?></p>
+
+<div class="container-fluid rounded shadow-sm p-4 bg-light">
+   <h2 class="text-dark "><?php echo html_escape($alert_title); ?></h2>
+    <p class="text-muted p-2"><?php echo html_escape($alert_message); ?></p>
     <button type="button" onclick="this.closest('dialog').close();">Close</button>
-</dialog>
+</div>
 <script>
     document.getElementById('application-alert').showModal();
 </script>
