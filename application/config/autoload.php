799c28bca314207f2a08c532507e49b15360c9b8
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-24 21:25:51 +0800

    Autoload centralized UI helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $autoload['packages'] = array();
 $autoload['libraries'] = array('database', 'session', 'form_validation');
 $autoload['drivers'] = array();
-$autoload['helper'] = array('url', 'form', 'html', 'file');
+$autoload['helper'] = array('url', 'form', 'html', 'file', 'ui');
 $autoload['config'] = array();
 $autoload['language'] = array();
 $autoload['model'] = array();
