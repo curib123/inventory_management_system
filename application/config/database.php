33428a4e0abc5313d3632d38b0615ebe00d122af
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-24 22:09:49 +0800

    Use environment-driven database configuration

--- application/config/database.php
+++ application/config/database.php
@@ -4,12 +4,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+$db_hostname = getenv('INVENTORY_DB_HOST');
+$db_username = getenv('INVENTORY_DB_USER');
+$db_password = getenv('INVENTORY_DB_PASSWORD');
+$db_database = getenv('INVENTORY_DB_NAME');
+$db_port = getenv('INVENTORY_DB_PORT');
+
 $db['default'] = array(
     'dsn'      => '',
-    'hostname' => 'localhost',
-    'username' => 'root',
-    'password' => '',
-    'database' => 'inventory_management_db', // Pwede ra nimo ilisan ni if lain imong database name.
+    'hostname' => ($db_hostname !== FALSE && $db_hostname !== '') ? $db_hostname : 'localhost',
+    'username' => ($db_username !== FALSE && $db_username !== '') ? $db_username : 'root',
+    'password' => ($db_password !== FALSE) ? $db_password : '',
+    'database' => ($db_database !== FALSE && $db_database !== '') ? $db_database : 'inventory_management_db',
     'dbdriver' => 'mysqli',
     'dbprefix' => '',
     'pconnect' => FALSE,
@@ -17,11 +23,12 @@ $db['default'] = array(
     'cache_on' => FALSE,
     'cachedir' => '',
     'char_set' => 'utf8mb4',
-    'dbcollat' => 'utf8mb4_general_ci',
+    'dbcollat' => 'utf8mb4_unicode_ci',
     'swap_precedence' => 'auto',
     'encrypt' => FALSE,
     'compress' => FALSE,
-    'stricton' => FALSE,
+    'stricton' => TRUE,
     'failover' => array(),
-    'save_queries' => TRUE
+    'save_queries' => (ENVIRONMENT !== 'production'),
+    'port' => ($db_port !== FALSE && $db_port !== '') ? (int) $db_port : 3306
 );
