3655cf545573dd2c7515788ab141ed523aa2d226
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-24 21:27:36 +0800

    Replace legacy alert dialog with centralized modal design

--- application/views/modal/alerts/message.php
+++ application/views/modal/alerts/message.php
@@ -1,9 +1,12 @@
+<?php
+$this->load->view('components/modal/header', array(
+    'modal_title' => $alert_title,
+    'modal_icon' => 'bi-info-circle'
+));
+?>
 
-<div class="container-fluid rounded shadow-sm p-4 bg-light">
-   <h2 class="text-dark "><?php echo html_escape($alert_title); ?></h2>
-    <p class="text-muted p-2"><?php echo html_escape($alert_message); ?></p>
-    <button type="button" onclick="this.closest('dialog').close();">Close</button>
+<div class="modal-body">
+    <p class="mb-0"><?php echo html_escape($alert_message); ?></p>
 </div>
-<script>
-    document.getElementById('application-alert').showModal();
-</script>
+
+<?php $this->load->view('components/modal/footer', array('close_label' => 'Close')); ?>
