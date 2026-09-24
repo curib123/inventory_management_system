1893c932818a767d4c16f337b91cd603aece64fe
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-24 22:51:36 +0800

    Harden stock integer validation

--- application/libraries/Stock_rules.php
+++ application/libraries/Stock_rules.php
@@ -6,19 +6,26 @@ class Stock_rules {
 
     const MAX_STOCK = 2147483647;
 
-    public function calculate_stock($current_stock, $quantity, $type) {
-        if (!in_array($type, array('stock_in', 'stock_out'), TRUE)) {
-            throw new InvalidArgumentException('Invalid stock transaction type.');
-        }
-
-        $current_stock = filter_var(
-            $current_stock,
+    public function validate_stock_value($stock) {
+        $stock = filter_var(
+            $stock,
             FILTER_VALIDATE_INT,
             array('options' => array(
                 'min_range' => 0,
                 'max_range' => self::MAX_STOCK
             ))
         );
+
+        if ($stock === FALSE) {
+            throw new InvalidArgumentException(
+                'Stock must be a valid whole number between 0 and ' . self::MAX_STOCK . '.'
+            );
+        }
+
+        return (int) $stock;
+    }
+
+    public function validate_quantity($quantity) {
         $quantity = filter_var(
             $quantity,
             FILTER_VALIDATE_INT,
@@ -28,17 +35,22 @@ class Stock_rules {
             ))
         );
 
-        if ($current_stock === FALSE) {
+        if ($quantity === FALSE) {
             throw new InvalidArgumentException(
-                'Current stock must be a valid non-negative whole number.'
+                'Quantity must be a valid whole number between 1 and ' . self::MAX_STOCK . '.'
             );
         }
 
-        if ($quantity === FALSE) {
-            throw new InvalidArgumentException(
-                'Quantity must be a valid whole number greater than zero.'
-            );
+        return (int) $quantity;
+    }
+
+    public function calculate_stock($current_stock, $quantity, $type) {
+        if (!in_array($type, array('stock_in', 'stock_out'), TRUE)) {
+            throw new InvalidArgumentException('Invalid stock transaction type.');
         }
+
+        $current_stock = $this->validate_stock_value($current_stock);
+        $quantity = $this->validate_quantity($quantity);
 
         if ($type === 'stock_out' && $quantity > $current_stock) {
             throw new UnderflowException('Insufficient stock.');
