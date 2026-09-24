f911fb7aa8eb0cf1c42e59c2debbdb81d805bfce
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-24 21:45:16 +0800

    Name authorization guard by permission key

--- application/controllers/Stock.php
+++ application/controllers/Stock.php
@@ -302,9 +302,9 @@ class Stock extends CI_Controller {
         return $items;
     }
 
-    private function require_permission($permission_name) {
+    private function require_permission($permission_key) {
         $user_id = $this->session->userdata('user_id');
-        if (!$user_id || !$this->User_model->has_permission($user_id, $permission_name)) {
+        if (!$user_id || !$this->User_model->has_permission($user_id, $permission_key)) {
             show_error('You do not have permission to access this page.', 403, 'Access Denied');
         }
     }
