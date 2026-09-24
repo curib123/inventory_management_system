6afa82b5b167f75243563327158025e3315a1893
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-24 21:41:47 +0800

    Hide create action without suppliers.create

--- application/views/suppliers/index.php
+++ application/views/suppliers/index.php
@@ -1,15 +1,19 @@
 <?php
+$page_actions = array();
+
+if ($this->User_model->has_permission($this->session->userdata('user_id'), 'suppliers.create')) {
+    $page_actions[] = array(
+        'label' => 'Add Supplier',
+        'icon' => 'bi-plus-lg',
+        'class' => 'btn-primary',
+        'modal_url' => site_url('suppliers/add')
+    );
+}
+
 $this->load->view('components/page_header', array(
     'title' => 'Suppliers',
     'description' => 'Manage supplier information and product sources.',
-    'actions' => array(
-        array(
-            'label' => 'Add Supplier',
-            'icon' => 'bi-plus-lg',
-            'class' => 'btn-primary',
-            'modal_url' => site_url('suppliers/add')
-        )
-    )
+    'actions' => $page_actions
 ));
 
 $this->load->view('components/data_table', array(
