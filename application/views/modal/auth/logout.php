41501d2b0ca529664100e6b340c296010b0246cd
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-24 22:43:19 +0800

    Centralize logout confirmation UX

--- application/views/modal/auth/logout.php
+++ application/views/modal/auth/logout.php
@@ -8,12 +8,22 @@ $this->load->view('components/modal/header', array(
 ?>
 
 <div class="modal-body">
-    <p class="mb-0">Are you sure you want to log out?</p>
+    <?php
+    $this->load->view('components/modal/confirmation', array(
+        'confirmation_variant' => 'warning',
+        'confirmation_icon' => 'bi-box-arrow-right',
+        'confirmation_title' => 'Log out now?',
+        'confirmation_message' => 'Your current session will end and you will need to sign in again to continue.',
+        'confirmation_items' => array(
+            'Make sure any unfinished form work has already been saved.'
+        )
+    ));
+    ?>
 </div>
 
 <?php
 $this->load->view('components/modal/footer', array(
-    'close_label' => 'Cancel',
+    'close_label' => 'Stay Signed In',
     'submit_label' => 'Logout',
     'submit_class' => 'btn-danger',
     'submit_icon' => 'bi-box-arrow-right'
