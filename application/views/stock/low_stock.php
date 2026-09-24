a505f340be2772581845537ec9da39104ce6b845
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-24 19:46:41 +0800

    Use centralized DataTable component for stock/low_stock.php

--- application/views/stock/low_stock.php
+++ application/views/stock/low_stock.php
@@ -1,14 +1,14 @@
-<h2>Low Stock Monitoring</h2>
-
-<table data-datatable-server data-source="<?php echo site_url('stock/low-stock/datatable'); ?>">
-    <thead>
-        <tr>
-            <th>Code</th>
-            <th>Product</th>
-            <th>Current Stock</th>
-            <th>Reorder Level</th>
-            <th>Unit</th>
-        </tr>
-    </thead>
-    <tbody></tbody>
-</table>
+<?php
+$this->load->view('components/datatable', array(
+    'title' => 'Low Stock Monitoring',
+    'subtitle' => 'Products that have reached or fallen below their reorder level.',
+    'data_source' => site_url('stock/low-stock/datatable'),
+    'columns' => array(
+        'Code',
+        'Product',
+        'Current Stock',
+        'Reorder Level',
+        'Unit'
+    )
+));
+?>
