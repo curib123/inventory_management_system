6661fe7957a15a59487c4aa934e6eb1271836b03
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-24 21:27:38 +0800

    Replace legacy logout dialog with centralized modal design

--- application/views/modal/auth/logout.php
+++ application/views/modal/auth/logout.php
@@ -1,6 +1,22 @@
-<dialog id="logout-confirmation">
-    <h3>Confirm Logout</h3>
-    <p>Are you sure you want to log out?</p>
-    <a href="<?php echo site_url('logout'); ?>">Logout</a>
-    <button type="button" onclick="this.closest('dialog').close();">Cancel</button>
-</dialog>
+<?php echo form_open('logout', array('data-modal-form' => '1')); ?>
+<?php
+$this->load->view('components/modal/header', array(
+    'modal_title' => 'Confirm Logout',
+    'modal_subtitle' => 'End the current signed-in session.',
+    'modal_icon' => 'bi-box-arrow-right'
+));
+?>
+
+<div class="modal-body">
+    <p class="mb-0">Are you sure you want to log out?</p>
+</div>
+
+<?php
+$this->load->view('components/modal/footer', array(
+    'close_label' => 'Cancel',
+    'submit_label' => 'Logout',
+    'submit_class' => 'btn-danger',
+    'submit_icon' => 'bi-box-arrow-right'
+));
+?>
+<?php echo form_close(); ?>
