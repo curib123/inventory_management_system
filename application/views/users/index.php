fd1dab2fdf546379878989954cb1c68ccc1fc7e4
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-26 07:54:45 +0800

    Use Authorization_service in index.php

--- application/views/users/index.php
+++ application/views/users/index.php
@@ -1,7 +1,7 @@
 <?php
 $page_actions = array();
 
-if ($this->User_model->has_permission($this->session->userdata('user_id'), 'users.create')) {
+if ($this->authorization_service->has_permission($this->session->userdata('user_id'), 'users.create')) {
     $page_actions[] = array(
         'label' => 'Add User',
         'icon' => 'bi-person-plus',
