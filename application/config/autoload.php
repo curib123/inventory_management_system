2cc67468137799b6019decfa508531a8750fb058
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-26 07:54:00 +0800

    Autoload centralized authorization service

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $autoload['packages'] = array();
-$autoload['libraries'] = array('database', 'session', 'form_validation');
+$autoload['libraries'] = array('database', 'session', 'form_validation', 'Authorization_service');
 $autoload['drivers'] = array();
 $autoload['helper'] = array('url', 'form', 'html', 'file', 'ui');
 $autoload['config'] = array();
