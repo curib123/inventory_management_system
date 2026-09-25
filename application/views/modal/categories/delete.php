f6b53b2b0c99a4f6f35862f36dd8e1eb57accabd
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-25 21:34:53 +0800

    Use danger SaaS treatment for category deletion

--- application/views/modal/categories/delete.php
+++ application/views/modal/categories/delete.php
@@ -3,7 +3,9 @@
 $this->load->view('components/modal/header', array(
     'modal_title' => 'Delete Category',
     'modal_subtitle' => 'Confirm this permanent action before continuing.',
-    'modal_icon' => 'bi-trash3'
+    'modal_icon' => 'bi-trash3',
+    'modal_variant' => 'danger',
+    'modal_eyebrow' => 'Destructive action'
 ));
 ?>
 
