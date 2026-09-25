c7e5ed8154834cb0d652e1877f4e7458ce92869b
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-25 21:11:53 +0800

    Add low-stock severity filter and badges

--- application/views/stock/low_stock.php
+++ application/views/stock/low_stock.php
@@ -7,10 +7,23 @@ $this->load->view('components/page_header', array(
 $this->load->view('components/data_table', array(
     'source' => site_url('stock/low-stock/datatable'),
     'table_id' => 'low-stock-table',
+    'search_placeholder' => 'Search product code, name, or unit...',
+    'filters' => array(
+        array(
+            'name' => 'severity',
+            'label' => 'Stock alert',
+            'icon' => 'bi-exclamation-triangle',
+            'options' => array(
+                '' => 'All low stock',
+                'out' => 'Out of stock',
+                'low' => 'Low but available'
+            )
+        )
+    ),
     'columns' => array(
         'Code',
         'Product',
-        array('label' => 'Current Stock', 'class' => 'text-end text-nowrap'),
+        array('label' => 'Current Stock', 'class' => 'text-end text-nowrap', 'render' => 'stock_alert'),
         array('label' => 'Reorder Level', 'class' => 'text-end text-nowrap'),
         array('label' => 'Unit', 'class' => 'text-nowrap')
     )
