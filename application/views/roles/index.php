1b8855c2d40b0cd8085756031586d901a2614b8d
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-26 07:54:42 +0800

    Use Authorization_service in index.php

--- application/views/roles/index.php
+++ application/views/roles/index.php
@@ -1,7 +1,7 @@
 <?php
 $page_actions = array();
 
-if ($this->User_model->has_permission($this->session->userdata('user_id'), 'roles.create')) {
+if ($this->authorization_service->has_permission($this->session->userdata('user_id'), 'roles.create')) {
     $page_actions[] = array(
         'label' => 'Add Role',
         'icon' => 'bi-shield-plus',
