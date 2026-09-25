039cb393f038025c7bfbb16ef01195415320d4ca
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-25 21:34:26 +0800

    Refine shared action modal container

--- application/views/modal/container.php
+++ application/views/modal/container.php
@@ -1,10 +1,11 @@
 <div
-    class="modal fade"
+    class="modal fade app-action-modal"
     id="action-modal"
     tabindex="-1"
     aria-hidden="true"
+    aria-labelledby="action-modal-title"
 >
-    <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
-        <div class="modal-content" id="action-modal-content"></div>
+    <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable app-modal-dialog">
+        <div class="modal-content app-modal-content" id="action-modal-content"></div>
     </div>
 </div>
