6e98f138b827f4d97ff14722685e415efb9b2dc0
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-26 11:11:01 +0800

    Load Authorization_service in PHPUnit bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,7 @@
 
 define('BASEPATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'system' . DIRECTORY_SEPARATOR);
 require_once dirname(__DIR__) . '/application/libraries/Auth_service.php';
+require_once dirname(__DIR__) . '/application/libraries/Authorization_service.php';
 require_once dirname(__DIR__) . '/application/libraries/Stock_rules.php';
 require_once dirname(__DIR__) . '/application/libraries/Report_rules.php';
 require_once dirname(__DIR__) . '/application/libraries/Datatable_service.php';
